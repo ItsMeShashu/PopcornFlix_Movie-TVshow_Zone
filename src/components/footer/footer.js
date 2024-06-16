79890c10291457df4819339032f485b43dc070f9
SHASHWAT  GARG <gargshashwat705@gmail.com>
2024-06-17 01:59:47 +0530

    Footer and header changed

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./footer.scss";
 
-import {
-  FaFacebookF,
-  FaInstagram,
-  FaTwitter,
-  FaLinkedin,
-} from "react-icons/fa";
+import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 
 import ContentWrapper from "../contentWrap/ContentWrap";
+
+const navigateTo = (url) => {
+  window.open(url, "_blank", "noopener,noreferrer");
+};
 
 const Footer = () => {
   return (
@@ -22,26 +21,35 @@ const Footer = () => {
           <li className="menuItem">FAQ</li>
         </ul>
         <div className="infoText">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
+          PopcornFlix is your ultimate destination for discovering the latest,
+          most popular, and top-rated movies and TV shows. Powered by the TMDb
+          API, we bring you an extensive collection of trending content to
+          explore. Stay updated with the newest releases and all-time favorites,
+          and enjoy your next binge-watch session with PopcornFlix. Happy
+          watching!
         </div>
+
         <div className="socialIcons">
-          <span className="icon">
-            <FaFacebookF />
+          <span
+            className="icon"
+            onClick={() => navigateTo("https://github.com/ItsMeShashu")}
+          >
+            <FaGithub />
           </span>
           <span className="icon">
             <FaInstagram />
           </span>
-          <span className="icon">
-            <FaTwitter />
-          </span>
-          <span className="icon">
+          <span
+            className="icon"
+            onClick={() =>
+              navigateTo("https://www.linkedin.com/in/shashwat-garg-9731a4281/")
+            }
+          >
             <FaLinkedin />
           </span>
+        </div>
+        <div className="infoText3">
+          © 2023 <strong>Shashwat Garg</strong>. All rights reserved.
         </div>
       </ContentWrapper>
     </footer>
